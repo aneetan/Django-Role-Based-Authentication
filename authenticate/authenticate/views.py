ca21b053cd08066ea2b6c1ceea4d8c574cc1c0cd
aneetan <anitaneupane2061@gmail.com>
2024-08-22 20:28:40 +0545

    Login token

--- authenticate/authenticate/views.py
+++ authenticate/authenticate/views.py
@@ -6,10 +6,22 @@ from rest_framework import status               #return status code 201 or 400
 from rest_framework.authtoken.models import Token  #fetch and create user and token
 from django.contrib.auth.models import User
 
+from django.shortcuts import get_object_or_404
+
 
 @api_view(['POST'])
 def login(request):
-    return Response({})
+    #return 404 if user is not found 
+    user = get_object_or_404(User, username = request.data['username'])
+
+    if not user.check_password(request.data['password']):
+        return Response({"detail": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+
+    token = Token.objects.get(user = user)
+    serializer = UserSerializer(instance = user )
+    return Response({"token": token.key, "user": serializer.data})
+
 
 @api_view(['POST'])
 def signup(request):
